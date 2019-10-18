6fccf95ea782b23a3d0856fd1de94dc3be16514c
Carlos Mitchell <kakismitchell@gmail.com>
2019-10-18 01:38:57 -0400

    top Menu added, Link and routers

--- chef-portfolio/src/App.js
+++ chef-portfolio/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
+
+
+
 function App() {
   return (
+    <Router>
     <div className="App">
      <NavBar/>
     </div>
+  </Router>
   );
 }
 
