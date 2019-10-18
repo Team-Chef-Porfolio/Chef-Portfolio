3c2f2d2e9e74fe95bdcd01f35c20c6a147fc8f57
Carlos Mitchell <kakismitchell@gmail.com>
2019-10-17 22:59:49 -0400

    file structure

---
+++ chef-portfolio/src/components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
+
+
+
+
+
+function NavBar (){
+    
+    
+    return (
+        <div className="NavBar">
+
+            
+
+          </div>
+                
+    );
+};
+
+export default NavBar;
