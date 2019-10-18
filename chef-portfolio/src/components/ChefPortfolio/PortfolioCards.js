b0941589746cafe625410e69ca206283010c924f
Miley Wright <dishaliekwright@yahoo.com>
2019-10-18 16:04:31 -0400

    finished entering in PortfolioCards file

---
+++ chef-portfolio/src/components/ChefPortfolio/PortfolioCards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PortfolioList from './PortfolioList';
+
+const PortfolioCards = (props) =>{
+    console.log(props)
+    return(
+      <PortfolioList className= 'portfolioBody'/>
+    );
+};
+
+export default PortfolioCards;
