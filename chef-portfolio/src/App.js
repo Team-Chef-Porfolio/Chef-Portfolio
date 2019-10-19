59f3ce576db1acdb53385ec9abc24e058c9baf47
Miley Wright <dishaliekwright@yahoo.com>
2019-10-18 20:05:00 -0400

    finished portfolios. Need to add font, color(if any), route these links

--- chef-portfolio/src/App.js
+++ chef-portfolio/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Router>
     <div className="App">
      <NavBar/>
-     
+     <PortfolioList/>
     </div>
   </Router>
   );
--- chef-portfolio/src/components/ChefPortfolio/PortfolioList.js
+++ chef-portfolio/src/components/ChefPortfolio/PortfolioList.js
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
+import './ChefPortfolio.css';
+import {Link} from 'react-router-dom';
+// import {Fromik, Form, Field} from 'formik';
 
 export default function PortfolioList() {
     const [chef, setChef] = useState([]);
@@ -16,18 +19,23 @@ export default function PortfolioList() {
     }, []);
     console.log(chef)
     return(
+    <div className='chef-container'>
+        <h1>Portfolios</h1>
     <section className = 'chef-list grid-view'>
     {chef.map(chef =>( 
         <div className = 'chefs' key={chef.id}>
+            <Link className="chef-link" to={ `/portfolio/${chef.chef_name}/${chef.id}`}>
             <img
             className='chef-photo'
             src = {chef.item_photo}
             alt = {chef.chef_name}
             />
-            <h2>{chef.chef_name}'s Recipes</h2>
+           
+            <h2>{chef.chef_name}'s Recipes</h2> </Link>
         </div>
         ))}
     
-    </section> 
+    </section>
+    </div> 
     )
 }
