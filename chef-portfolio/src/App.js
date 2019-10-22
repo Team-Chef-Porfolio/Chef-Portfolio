6d5373593043a08e48c25cf09e1f62d61fcb5288
Jeris Manning <the.goose.moose@gmail.com>
2019-10-22 15:05:21 -0600

    AddRecipe Component functional, updated PortfolioList to show all recipes/chefs for testing, Id added to localstorage on register for testing

--- chef-portfolio/src/App.js
+++ chef-portfolio/src/App.js
@@ -3,33 +3,34 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Login/Login';
 import RegisterChef from './components/RegisterChef/RegisterChef';
+import AddRecipe from './components/AddRecipe/AddRecipe';
 import Support from './components/support/Support';
 import Account from './components/account/Account';
 import Portfolio from './components/portfolio/Portfolio';
 import About from './components/about/About';
 import './App.css';
-import NavBar from "./components/NavBar/NavBar";
+import NavBar from './components/NavBar/NavBar';
 import PortfolioList from './components/ChefPortfolio/PortfolioList';
 import RecipeList from './components/ChefPortfolio/RecipeList';
 
-
-
 function App() {
-  
   return (
     <Router>
-    <div className="App">
-     <NavBar/>
-     <Switch>
-     <PrivateRoute exact path="/Portfolio" component={Portfolio} />
-     <PrivateRoute path="/account" component={Account} />
-     <PrivateRoute path="/about" component={About} />
-     <Route path="/login" component={Login} />
-     <Route path="/support" component={Support} />
-     <Route path="/registerchef" component={RegisterChef} />
-     </Switch>
-    </div>
-  </Router>
+      <div className='App'>
+        <NavBar />
+
+        <Switch>
+          <PrivateRoute exact path='/Portfolio' component={Portfolio} />
+          <PrivateRoute path='/account' component={Account} />
+          <PrivateRoute path='/about' component={About} />
+          <PrivateRoute path='/addrecipe' component={AddRecipe} />
+          <Route exact path='/' component={PortfolioList} />
+          <Route path='/login' component={Login} />
+          <Route path='/support' component={Support} />
+          <Route path='/registerchef' component={RegisterChef} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
