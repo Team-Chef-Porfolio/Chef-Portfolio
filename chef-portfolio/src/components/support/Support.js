eb718849758799983ff81929a610f8207ab6ebd0
Miley Wright <dishaliekwright@yahoo.com>
2019-10-22 20:40:25 -0400

    support form completed. Adding styling

--- chef-portfolio/src/components/support/Support.js
+++ chef-portfolio/src/components/support/Support.js
@@ -1,37 +1,44 @@
-import React, {useState, useEffect} from 'react';
-import {withFormik, Form, Field} from 'formik';
-import Account from '../account/Account'
+import React, {useState} from 'react';
 
 
-const Support = (props) => {
-   const [support, setSupport] = useState([]);
-   
-   useEffect(() => {
-     if(props.status){
-        setSupport([...support, props.status]);
-     } 
-   },[props.status]);
- return (
-   
-   <div className='support-form'><h1>SUPPORT</h1>
-      <Form>
-         <Field type='text' name= 'name' placeholder='Name' />
-         {props.touched.name && props.errors.name && (
-            <p className= 'errors'> {props.errors.name} </p>
-         )}
-         <Field component = 'select' name='supportOptions' className='supportOptions' >
-            <option value='account'> Account </option>
-            <option value='forgot'> Forgot </option>
-            <option value='change'> Change </option>
-         </Field>
-         <Field type='text' name='additional' placeholder='' />
-         {props.touched.additional && props.errors.additional &&(
-            <p className='errors'> {props.errors.additional} </p>
-         )} 
-         <button type='submit'> Send Email</button>
-      </Form>
-   </div>
- )
+const Support = () => {
+   const defaultSupport = {
+      name: '',
+      additional: ''
+   }
+
+   const [support, setSupport] = useState({defaultSupport});
+   const handleChange = e => {
+      setSupport({...support, [e.target.name]: e.target.value})
+   }
+   return(
+      <form className='supportContainer'>
+         <label htmlFor = 'supportName'> Name </label>
+         <input 
+         id= 'name' 
+         name= 'name'
+         type='text'
+         value={support.name}
+         onChange={handleChange}/>
+         
+         <label htmlFor = 'select'>What can we help you with?</label>
+         <select className='supportDropdown'
+         name= 'select'>
+         <option value='Account'>Account</option>
+         <option value='Forgot'>Forgot</option>
+         <option value='Change'>Change</option>
+         </select>
+         
+         
+         <label htmlFor = 'supportAdditional'>Additional</label>
+         <input 
+         id= 'additional' 
+         name= 'additional'
+         type='text'
+         value={support.additional}
+         onChange={handleChange}/>
+      </form> 
+   )
 }
 
 export default Support;
