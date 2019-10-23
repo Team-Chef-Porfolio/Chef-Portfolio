e413a6407613e568192af4fec2919844dcb2391e
Miley Wright <dishaliekwright@yahoo.com>
2019-10-22 23:15:03 -0400

    support page is completed

--- chef-portfolio/src/components/support/Support.js
+++ chef-portfolio/src/components/support/Support.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import './Support.css';
 
 const Support = () => {
    const defaultSupport = {
@@ -12,32 +12,41 @@ const Support = () => {
       setSupport({...support, [e.target.name]: e.target.value})
    }
    return(
-      <form className='supportContainer'>
-         <label htmlFor = 'supportName'> Name </label>
-         <input 
+      <div className='support'>
+      <div className='supportContainer'>
+         <h1 className= 'title'>Support</h1>
+      <form >
+         <div className="inputContainer">
+         <p><label htmlFor = 'supportName'> Name </label></p>
+         <input className='nameField'
          id= 'name' 
          name= 'name'
          type='text'
          value={support.name}
          onChange={handleChange}/>
-         
-         <label htmlFor = 'select'>What can we help you with?</label>
+         </div>
+         <div className='inputContainer'>
+         <p><label htmlFor = 'select'>What can we help you with?</label></p>
          <select className='supportDropdown'
          name= 'select'>
          <option value='Account'>Account</option>
          <option value='Forgot'>Forgot</option>
          <option value='Change'>Change</option>
          </select>
-         
-         
-         <label htmlFor = 'supportAdditional'>Additional</label>
-         <input 
+         </div>
+
+         <div className='inputContainer'>
+         <p><label htmlFor = 'supportAdditional'>Additional</label></p>
+         <textarea className='lastField'
          id= 'additional' 
          name= 'additional'
-         type='text'
          value={support.additional}
          onChange={handleChange}/>
+         </div>
+         <div className='button'><button>Contact Us</button></div>
       </form> 
+      </div>
+      </div>
    )
 }
 
