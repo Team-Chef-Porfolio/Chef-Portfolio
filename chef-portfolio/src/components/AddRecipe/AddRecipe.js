fcce866e570ada4003704da2c7affb4a5468c10d
Jeris Manning <the.goose.moose@gmail.com>
2019-10-24 17:30:52 -0600

    AddRecipe component now adds recipe to server where available, adds other attributes to local storage like the pros do

--- chef-portfolio/src/components/AddRecipe/AddRecipe.js
+++ chef-portfolio/src/components/AddRecipe/AddRecipe.js
@@ -1,30 +1,38 @@
 import React, { useState } from 'react';
 import { axiosAuthenticate } from '../axiosAuthenticate/axiosAuthenticate';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const RecipeContainer = styled.div`
-
-border: blue solid 3px;
-width: 800px;
-height: 100vh;
-margin: 0 auto;
-
-`
-
-
-
+  border: blue solid 3px;
+  width: 800px;
+  height: 100vh;
+  margin: 0 auto;
+`;
 
 const AddRecipe = () => {
   const defaultRecipe = {
-    id: "",
+    id: '',
     chef_name: '',
-    recipe_title: "",
-    item_photo: "",
-    chef_location: "",
-    item_ingredients: "",
-    user_id: ""
+    recipe_title: '',
+    item_photo: '',
+    chef_location: '',
+    item_ingredients: '',
+    user_id: ''
+    
   };
+
+const defaultLocalRecipe = {
+  recipe_instructions: '',
+  cuisine: ''
+}
+
   const [recipe, setRecipe] = useState(defaultRecipe);
+
+  const [localRecipe, setLocalRecipe] = useState(defaultLocalRecipe);
+
+  const handleLocalChange = (e) => {
+    setLocalRecipe({ ...localRecipe, [e.target.name]: e.target.value });
+  };
 
   const handleChange = (e) => {
     setRecipe({ ...recipe, [e.target.name]: e.target.value });
@@ -32,75 +40,103 @@ const AddRecipe = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     axiosAuthenticate()
       .post('/posts/', recipe)
       .then((res) => {
         console.log(res);
         setRecipe(defaultRecipe);
+        localStorage.setItem('instructions', localRecipe.recipe_instructions);
+        localStorage.setItem('cuisine', localRecipe.cuisine);
+        setLocalRecipe(defaultLocalRecipe);
       })
       .catch((err) => console.log(err));
   };
 
   return (
     <RecipeContainer>
-    <form onSubmit={handleSubmit}>
-      
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='chef_name'
+          value={recipe.chef_name}
+          onChange={handleChange}
+          placeholder="Chef's Name"
+        />
 
-      <input
-        type='text'
-        name='chef_name'
-        value={recipe.chef_name}
-        onChange={handleChange}
-        placeholder="Chef's Name"
-      />
+        <input
+          type='text'
+          name='recipe_title'
+          value={recipe.recipe_title}
+          onChange={handleChange}
+          placeholder='Recipe Title'
+        />
 
-      <input
-        type='text'
-        name='recipe_title'
-        value={recipe.recipe_title}
-        onChange={handleChange}
-        placeholder='Recipe Title'
-      />
+        <input
+          type='url'
+          name='item_photo'
+          value={recipe.item_photo}
+          onChange={handleChange}
+          placeholder='Photo URL'
+        />
 
-      <input
-        type='url'
-        name='item_photo'
-        value={recipe.item_photo}
-        onChange={handleChange}
-        placeholder='Photo URL'
-      />
+        <input
+          type='text'
+          name='item_ingredients'
+          value={recipe.item_ingredients}
+          onChange={handleChange}
+          placeholder='Recipe Ingredients'
+        />
 
-      <input
-        type='text'
-        name='item_ingredients'
-        value={recipe.item_ingredients}
-        onChange={handleChange}
-        placeholder='Recipe Ingredients'
-      />
+        <input
+          type='text'
+          name='cuisine'
+          value={localRecipe.cuisine}
+          onChange={handleLocalChange}
+          placeholder='Cuisine'
+        />
 
-      <input
-        type='text'
-        name='user_id'
-        value={recipe.user_id}
-        onChange={handleChange}
-        placeholder='User ID'
-      />
+<input
+          type='text'
+          name='chef_location'
+          value={recipe.chef_location}
+          onChange={handleChange}
+          placeholder='Location'
+        />
 
+        <textarea
+          name=''
+          id=''
+          cols='30'
+          rows='10'
+          wrap='hard'
+          name='recipe_instructions'
+          value={localRecipe.recipe_instructions}
+          onChange={handleLocalChange}
+          placeholder='Recipe Instructions'></textarea>
 
+        <input
+          type='text'
+          name='id'
+          value={recipe.id}
+          onChange={handleChange}
+          placeholder='User ID'
+        />
 
-      <button type='submit'>Add Recipe</button>
-    </form>
+        <button type='submit'>Add Recipe</button>
+      </form>
     </RecipeContainer>
   );
 };
 
 export default AddRecipe;
 
-
-{/* <input
+{
+  /* <input
         type='text'
         name='id'
         value={recipe.id}
         onChange={handleChange}
         placeholder='ID'
-      /> */}
+      /> */
+}
